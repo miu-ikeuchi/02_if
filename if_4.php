65097b824171eaf781f14ba6e45e08f57f74554f
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-02-08 13:25:04 +0900

    if_4 2nd_cm

--- if_4.php
+++ if_4.php
@@ -6,7 +6,7 @@ $score_english = 24;
 if ($score_math >= 60 && $score_english >= 60) {
     echo '合格';
 } elseif ($score_math >= 50 || $score_english >= 50) {
-    echo '追試';
+    echo '再試験';
 } else {
     echo '不合格';
 }

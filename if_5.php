76ca9d037c138d4188dd2056a02269de744d8648
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-02-09 21:34:03 +0900

    if_5 4th_cm

--- if_5.php
+++ if_5.php
@@ -4,10 +4,11 @@ $num = 23;
 
 for ($i=2; $i<=$num; $i++) { 
     if ($num % $i == 0) {
-    echo $num . 'は素数ではありません';
-    break;
-    } elseif ($num % $i == 1){
-    echo $num . 'は素数です';
-    break;
+        echo $num . 'は素数ではありません';
+        break;
+    } 
+    elseif ($num % $i == 1){
+        echo $num . 'は素数です';
+        break;
     } 
 }

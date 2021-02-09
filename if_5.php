929391af9f5c91dc171ed91c0c72111f48ad1f54
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-02-09 20:52:53 +0900

    if_5 2nd_cm

--- if_5.php
+++ if_5.php
@@ -2,8 +2,11 @@
 
 $num = 10;
 
-if ($num % 2 == 0) {
+for ($i=2; $i<=$num; $i++) { 
+    if ($num % $i == 0) {
     echo $num . 'は素数ではありません';
-} else {
+    break;
+    } else {
     echo $num . 'は素数です';
+    } 
 }

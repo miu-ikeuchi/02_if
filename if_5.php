07b5f1cc8b3ac2055cc562ddbc9f1c945040d17c
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-02-11 22:57:40 +0900

    if_5 5th_cm

--- if_5.php
+++ if_5.php
@@ -2,10 +2,10 @@
 $num =15;
 
 $flg = true;
-for($i=2;$i<$num;$i++) {
+for ($i = 2; $i < $num; $i++) {
     if($num%$i == 0) {
-    $flg = false;
-    break;
+        $flg = false;
+        break;
     }
 }
 if ($flg) {
